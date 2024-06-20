3b5ce066efad6cbe28a8528bb612d0038923fbf1
pedro vargas <phvs2005@outlook.com>
2024-06-20 16:43:01 -0300

    planejamento inicial

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "TARVBM/TARVBM.h"
+
+// transformar a informação em 284 arqvs binarios
+
+// criar a arvore b+ dos arqvs binarios
+
+// criar 11 tabelas hash ordenadas (em ms) cada uma por um parametro diferente e com a informação do endereço de cada jogador na árvore
+
+// fazer as funções de busca de preferencia em um arquivo separado apenas para essas funções de busca
